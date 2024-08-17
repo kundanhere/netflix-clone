af0b95159f864cd8ee03e2673bb1edfff323a79e
Kundan Gupta <kundankumar8377@gmail.com>
2024-08-17 20:17:51 +0530

    setup routes, tailwindcss, framer-motion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,41 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+import SingUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <>
-      <h1>Hello From React</h1>
-    </>
+    <div
+      className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 
+    flex items-center justify-center relative overflow-hidden"
+    >
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<SingUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
