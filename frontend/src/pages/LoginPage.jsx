8d4f01371bdd5738b3c3601c62798483e5fe0c17
Kundan Gupta <kundankumar8377@gmail.com>
2024-08-18 07:43:34 +0530

    add login page ui

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,90 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../components/Input";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // TEMPORARY
+  const isLoading = false;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // TODO: Add login logic
+    try {
+      navigate("/verify/email");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Welcome Back
+        </h2>
+        <form onSubmit={handleLogin}>
+          <Input
+            icon={Mail}
+            type="text"
+            placeholder="Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="flex items-center mb-6">
+            <Link
+              to="/forgot/password"
+              className="text-sm text-green-400 hover:underline"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          {/* {error && <p className='text-red-500 font-semibold mb-2'>{error}</p>} */}
+          <motion.button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-green-600
+						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <Loader className="size-6 animate-spin mx-auto" />
+            ) : (
+              "Login"
+            )}
+          </motion.button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Don&apos;t have an account?{" "}
+          <Link to={"/signup"} className="text-green-400 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default LoginPage;
